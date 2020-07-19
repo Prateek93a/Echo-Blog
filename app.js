6e543963a54e4b832d88c8a60fa307da239d1382
Prateek93a <prateek93a@gmail.com>
2020-07-19 13:18:55 +0530

    added-dotenv-and-minor-changes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express=require("express");
+require('dotenv').config();
+const express = require("express");
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const routes=require('./routes/auth-routes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const bodyParser=require('body-parser');
+
+const routes = require('./routes/auth-routes');
 const profileRoutes = require('./routes/profile-routes');
 const displayRoutes = require('./routes/display-routes');
 const uploadRoutes = require('./routes/upload-routes');
 const exploreRoutes = require('./routes/explore-routes');
 const passportSetup = require('./config/passport-setup');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const bodyParser=require("body-parser");
 
-const app=express();
+const app = express();
 app.set("view-engine","ejs");
 
 // set up session cookies
@@ -31,7 +33,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // connect to mongodb
-mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true },()=>console.log("connected"));
+mongoose.connect(keys.mongodb.dbURI,{useNewUrlParser: true},() => console.log("connected"));
 mongoose.Promise=global.Promise;
 
 app.use("/auth",routes);
@@ -42,12 +44,12 @@ app.use("/display",displayRoutes);
 
 app.use(express.static("static"));
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res) => {
     if(req.user){
         res.redirect("/profile");
     }
     else{
-    res.render("home.ejs",{user:req.user});}
+    res.render("home.ejs",{user: req.user});}
 })
 
 app.listen(process.env.port||3000);
