4535e34fd6dc24ef2dcb3495a52c1f2388c80c15
Prateek93a <prateek93a@gmail.com>
2020-07-19 18:03:11 +0530

    added-callback-uri

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -18,7 +18,7 @@ passport.use(
         // options for google strategy
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: process.env.CALLBACK_URI
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db
         User.findOne({googleId: profile.id}).then((currentUser) => {
