4c0c042aeeb7123bcd6f3d402b1bf54b9067fa5e
Prateek Jain <prateek93a@gmail.com>
2020-07-22 08:44:48 +0530

    update user-blog-responses.js

--- models/user-blog-responses.js
+++ models/user-blog-responses.js
@@ -4,23 +4,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     blogId:String,
     writer:String,
-        profilepic:String,
-        date:String,
-        content:String
-    // responses: {
-    //     writer:String,
-    //     profilepic:String,
-    //     date:String,
-    //     content:String
-    // }work on this
-    
-   // googleId:String, implement it so you can view someone else's profile
-    
-  
-    
-    
-  
-  
+    profilepic:String,
+    date:String,
+    content:String
 });
 
 const Userblogresponse = mongoose.model('user-blog-response', userSchema);
